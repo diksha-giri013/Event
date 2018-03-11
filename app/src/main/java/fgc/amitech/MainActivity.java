bbfcedb0a7d7e66f4278fb20e2cc754778d0a2de
ZenithPal <32988114+ZenithPal@users.noreply.github.com>
2018-03-11 13:49:48 +0530

    commit

--- app/src/main/java/fgc/amitech/MainActivity.java
+++ app/src/main/java/fgc/amitech/MainActivity.java
@@ -1,33 +1,105 @@
 package fgc.amitech;
 
+
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
     private RecyclerView.ViewHolder viewHolder;
-List<event> eventList;
+    List<event> eventList;
+    String change_text[]={"2018 is here","have fun!!","it is fun!!","Let's explore","let do it","Let's participate"};
+    private TextSwitcher mSwitcher;
+    private int mCounter = 0;
+    private static  int SPLASH_TIME_OUT=3000;
+    private Handler handler = new Handler();
+
+    // Define the code block to be executed
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Insert custom code here
+            Random rand = new Random();
+            int  mCounter = rand.nextInt(6);
+            mSwitcher.setText(change_text[mCounter]);
+            // Repeat every 2 seconds
+            handler.postDelayed(runnable, SPLASH_TIME_OUT);
+        }
+    };
+
+// Start the Runnable immediately
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        RecyclerView rv=(RecyclerView)findViewById(R.id.mainRecycle);
+        RecyclerView rv=(RecyclerView)findViewById(R.id.dash_rv_eventcard);
         rv.setHasFixedSize(true);
         layoutManager=new LinearLayoutManager(this);
-        rv.setLayoutManager(layoutManager);*/
+        rv.setLayoutManager(layoutManager);
+       mSwitcher = (TextSwitcher) findViewById(R.id.switcher);
 //ContentAdapter adapter=new ContentAdapter(rv.getContext(),eventList);
+        mSwitcher.setFactory(mFactory);
+        Animation in = AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_in_left);
+        Animation out = AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_out_right);
+        mSwitcher.setInAnimation(in);
+        mSwitcher.setOutAnimation(out);
 
-    }}
+        mSwitcher.setCurrentText("2018 is here");
+
+        /*new Handler().postDelayed(new Runnable() {
+            @Override
+
+            public void run() {
+                Random rand = new Random();
+                int  mCounter = rand.nextInt(5);
+                mSwitcher.setText(change_text[mCounter]);
+
+                //finish();
+            }
+        },SPLASH_TIME_OUT);*/
+        handler.post(runnable);
+
+    }
+    private ViewSwitcher.ViewFactory mFactory = new ViewSwitcher.ViewFactory() {
+
+
+        @Override
+        public View makeView() {
+
+            // Create a new TextView
+            TextView t = new TextView(MainActivity.this);
+            t.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+/*            t.setTypeface(Typeface.defaultFromStyle(R.font.oxygen_light));*/
+            //t.setTextAppearance(MainActivity.this,R.font.chewy);
+            t.setTextAppearance(MainActivity.this, R.style.CustomText);
+
+            return t;
+    }
+    };}
 
 
     /*private class ContentAdapter extends RecyclerView.Adapter<ViewHolder>{
