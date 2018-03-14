e1497776dc584e3ffe4aba25c13e8f7418a85051
diksha-giri013 <31000182+diksha-giri013@users.noreply.github.com>
2018-03-14 23:18:47 +0530

    commit by Diksha

--- app/src/main/java/fgc/amitech/EventDetailActivity.java
+++ app/src/main/java/fgc/amitech/EventDetailActivity.java
@@ -1,7 +1,9 @@
 package fgc.amitech;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,6 +15,7 @@ public class EventDetailActivity extends AppCompatActivity {
     private TextView detail_title;
     private static int card_pos;
     private Button btn_next;
+    private Button btn_nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +24,16 @@ public class EventDetailActivity extends AppCompatActivity {
 
         detail_title = (TextView) findViewById(R.id.detail_tv_evname);
         detail_title.setText(card_data.getCard_title());
-        }
+
+        btn_nav = (Button) findViewById(R.id.detail_btn_nav);
+    }
+
+    public void navclick(View view){
+            Intent map = new Intent(EventDetailActivity.this, MapsActivity.class);
+            startActivity(map);
+   }
+
+
    public static void getCardData(EventDetails card, int pos){
         card_data=card;
         card_pos=pos;
