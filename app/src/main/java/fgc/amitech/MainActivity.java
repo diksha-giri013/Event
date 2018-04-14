a621380eac7528f845655ea140611b5d49ff5cf9
diksha-giri013 <31000182+diksha-giri013@users.noreply.github.com>
2018-04-14 17:52:42 +0530

    commit by Diksha

--- app/src/main/java/fgc/amitech/MainActivity.java
+++ app/src/main/java/fgc/amitech/MainActivity.java
@@ -137,7 +137,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                                 startActivity(new Intent(MainActivity.this,SponsorActivity.class));  break;
 
                             case R.id.events: //i=new Intent(MainActivity.this,EventActivity.class);
-                                startActivity(new Intent(MainActivity.this,EventActivity.class));  break;
+                                Snackbar snackbar2 = Snackbar.make(findViewById(R.id.dash_rv_catlist),"Updates in progress!",Snackbar.LENGTH_SHORT);
+                                snackbar2.show();
+                                break;
+                                //startActivity(new Intent(MainActivity.this,EventActivity.class));  break;
                             case R.id.developer: //i=new Intent(MainActivity.this,EventActivity.class);
                                 startActivity(new Intent(MainActivity.this,DeveloperActivity.class));  break;
                             //    newGame();
